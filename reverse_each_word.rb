d3e23db3153a055454ee34a912fc3c04c878e351
Chloe Gell <gellert.chloe@gmail.com>
2020-01-21 23:02:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
   newArr = []
-  word.each do |ele|
+  words.each do |ele|
     newArr << ele.reverse
   end
   return newArr.join(" ") 
